15c08e04f26a89bc3fef85fcc364f7613f4649a6
KangRuiYu <48875810+KangRuiYu@users.noreply.github.com>
2020-11-02 17:24:21 -0800

    Renamed case of files

---
+++ Scripts/level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _ready():
+	#check_hittable()
+	#print("do you even work bro?")
+	pass # Replace with function body.
+
+#func check_hittable():
+#	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
