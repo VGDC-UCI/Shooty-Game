3348015246b92d26f77f1a8b12e0d7a1fd89a9b9
KangRuiYu <48875810+KangRuiYu@users.noreply.github.com>
2020-11-20 21:19:37 -0800

    Organized files structure

---
+++ src/main/menus/title/main_menu.gd
@@ -0,0 +1,23 @@
+"""
+The main menu UI. Manages switching scenes to other menus.
+
+Author: Kang Rui Yu
+"""
+extends Control
+
+
+# References
+const SERVER_MENU_PATH: String = 'res://src/main/menus/multiplayer/ServerMenu.tscn'
+const LOCAL_MULTI_PATH: String = 'res://src/main/menus/local_game/LocalMultiplayerScreen.tscn'
+
+
+func switch_to_localmulti() -> void:
+	get_tree().change_scene(LOCAL_MULTI_PATH)
+
+
+func switch_to_multi() -> void:
+	get_tree().change_scene(SERVER_MENU_PATH)
+
+
+func switch_to_training() -> void:
+	pass
